2c9ff12618ddaa00c5e95fd154c53e6946768258
Николай Завражнов <niko3avr@yandex.ru>
2022-08-11 11:40:25 +0400

    3rd commit

--- suport_files/base_code.py
+++ suport_files/base_code.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from requests_info_vk_fun import get_info_owner, get_photo
-
-
+from requests_info_vk_fun import get_info_owner, get_photo, get_info_bot_user, compare_parametrs
+from pprint import pprint
+import time
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api import VkUpload
@@ -18,18 +18,23 @@ upload = VkUpload(vk)
 if __name__ == '__main__':
 
     for event in longpoll.listen():
+
         if event.type == VkEventType.MESSAGE_NEW:
 
             if event.to_me:
-                request = event.text
+                request = event.text.lower()
+                print(event.user_id)
+                info_bot_user = get_info_bot_user(os.getenv('VK_MYTOKEN'), event.user_id)
 
                 if request == "привет":
-                    info_owner = get_info_owner(os.getenv('VK_MYTOKEN'))
-                    attachments = get_photo(os.getenv('VK_MYTOKEN'), info_owner['response'][0]['id'])
+
+                    comparison = compare_parametrs(os.getenv('VK_MYTOKEN'), info_bot_user)
+                    attachments = get_photo(os.getenv('VK_MYTOKEN'), comparison['response'][0]['id'])
+
                     write_msg_attachment(event.user_id, f"Привет!, Вот,что мы подобрали для вас!\n"
-                                                        f"{info_owner['response'][0]['first_name']}"
-                                                        f" {info_owner['response'][0]['last_name']}\n"
-                                                        f"https://vk.com/{info_owner['response'][0]['domain']}",
+                                                        f"{comparison['response'][0]['first_name']}"
+                                                        f" {comparison['response'][0]['last_name']}\n"
+                                                        f"https://vk.com/{comparison['response'][0]['domain']}",
                                          vk_authoriz=vk, attachments=attachments)
 
 
--- suport_files/requests_info_vk_fun.py
+++ suport_files/requests_info_vk_fun.py
@@ -1,4 +1,4 @@
-
+import time
 from pprint import pprint
 from random import randrange
 
@@ -7,21 +7,26 @@ import requests
 from dotenv import load_dotenv, find_dotenv
 
 
-def get_photo(access_token, owner_id, count=100):
-
+def get_photo(access_token, owner_id, count=15):
+    # поиск трех популярных фото и формирования списка для attachment
     dict_photo = {}
     URl = 'https://api.vk.com/method/photos.get'
-    params = {'album_id': 'profile', 'access_token': access_token, 'extended': 1, 'rev': 0, 'owner_id': owner_id, 'v': '5.131', 'count': count}
+
+    params = {'album_id': 'profile', 'access_token': access_token,
+              'extended': 1, 'rev': 0, 'owner_id': owner_id, 'v': '5.131', 'count': count}
     res = requests.get(URl, params=params)
-    # pprint(res.json())
+    time.sleep(0.5)
+    pprint(res.json())
 
 
     if 'response' in res.json().keys() and\
             len(res.json()['response']['items']) > 0 and\
             'error' not in res.json().keys():
+
         for photos in res.json()['response']['items']:
             dict_photo[photos['likes']['count'], photos['id']] = f"photo{photos['owner_id']}_{photos['id']}"
             # print(f"{photos['likes']['count']} - {photos['sizes'][2]['url']}")
+
         pprint(dict_photo)
 
         list_send_photo = []
@@ -37,27 +42,69 @@ def get_photo(access_token, owner_id, count=100):
         if 0 < len(sorted(dict_photo)) <= 1:
             list_send_photo.append(dict_photo[sorted(dict_photo)[len(sorted(dict_photo)) - 1]])
 
-        print(list_send_photo)
+        # print(list_send_photo)
         print(','.join(list_send_photo))
         return ','.join(list_send_photo)
 
+    if 'response' in res.json().keys() and \
+            len(res.json()['response']['items']) == 0:
+
+        print('ФОТО СООБЩЕСТВА photo-214911415_457239017')
+        return 'photo-214911415_457239017'
 
 
-def get_info_owner(access_token, id_user='13708102'):
 
+
+
+def get_info_owner(access_token):
+    # поиск информации о пользователе для отправки сообщения
     URl = 'https://api.vk.com/method/users.get'
     params = {'access_token': access_token, 'fields': 'bdate,sex,city,domain',
-              'user_ids': randrange(10 ** 7), 'v': '5.131'}
+              'user_ids': randrange(10**7), 'v': '5.131'}
     res = requests.get(URl, params=params, )
-    dict_res = res.json()
-    if 'deactivated' not in dict_res['response'][0].keys():
-        get_photo(os.getenv('VK_MYTOKEN'), dict_res['response'][0]['id'])
-        pprint(dict_res)
+
+    if 'response' in res.json().keys() and\
+            'deactivated' not in res.json()['response'][0].keys()\
+            and res.json()['response'][0]['is_closed'] is False:
+
+        get_photo(os.getenv('VK_MYTOKEN'), res.json()['response'][0]['id'])
+        pprint(res.json())
+
 
         return res.json()
     else:
-        get_info_owner(access_token)
+        return get_info_owner(access_token)
+
+
+def get_info_bot_user(access_token, event_user_id):
+    # информация о пользователе, которому отправляется сообщение
+    URl = 'https://api.vk.com/method/users.get'
+    params = {'access_token': access_token, 'fields': 'bdate,sex,city,domain',
+              'user_ids': event_user_id, 'v': '5.131'}
+    res = requests.get(URl, params=params,)
+    pprint(res.json())
+
+    return res.json()
+
+def compare_parametrs(access_token, info_bot_user):
+    # сравнивет аналогичные параметры и находит пользователя при совпадении
+    info_owner = get_info_owner(access_token)
+
+    if info_owner['response'][0]['sex'] != info_bot_user['response'][0]['sex']:
+
+        return info_owner
+    else:
+        return compare_parametrs(access_token, info_bot_user)
+
+
+
+
+
 
 if __name__ == '__main__':
     load_dotenv(find_dotenv())
-    get_info_owner(os.getenv('VK_MYTOKEN'))
+    # get_photo(os.getenv('VK_MYTOKEN'), 81658)
+    info_ow = get_info_owner(os.getenv('VK_MYTOKEN'))
+    # print(info_ow)
+    # info_bot_user = get_info_bot_user(os.getenv('VK_MYTOKEN'), 9668538)
+    # print(f"{info_bot_user['response'][0]['city']['title']}\n{info_bot_user['response'][0]['sex']}")
