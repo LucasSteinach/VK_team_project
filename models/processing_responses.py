2c3b334e8c1dc8f60936a28cdda001a924996e99
Andrey Khudoyarov <andrei.hudojaroff@yandex.ru>
2022-08-18 12:04:55 +0400

    add doc

--- models/processing_responses.py
+++ models/processing_responses.py
@@ -1,10 +1,9 @@
-
-
 def determine_gender(response_json):
-# определяет противоположный пол для ответов полученных методом users.get
-# принимает в качестве аргумента словарь (res.json())
-# с параметром fields: 'sex'
-
+    """
+    Определяет противоположный пол для ответов полученных методом users.get
+    :param response_json Информация о пользователе в формате json
+    :return Функция возвращает цифру противоположного пола.
+    """
     if response_json['response'][0]['sex'] == 1:
         return 2
     elif response_json['response'][0]['sex'] == 2:
